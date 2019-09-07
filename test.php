b481dc8a15a70dacec6145831709ba8b27497d8f
sadsheep <sadsheep@mail.ru>
2019-09-07 21:32:30 +0300

    c

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php 
-$output = shell_exec('cat flag.php'); 
-$test = shell_exec('ls');
-echo "<pre>
 <?php 
+<?php 
+<?php 
+<?php<?php
 $output = shell_exec('cat flag.php'); 
 $test = shell_exec('ls');
-echo "<pre>$output</pre>"; 
+echo "<pre>$output</pre>";<?php
 echo 1+1
 echo $test
 ?> 
